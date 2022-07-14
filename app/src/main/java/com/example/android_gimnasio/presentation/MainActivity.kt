d3aee2052175407abeada5aa821df6515c952e2e
Carlos Vargas Huaman <cvargas@godaddy.com>
2022-07-13 19:11:47 -0500

    Se agrego segunda actividad

--- app/src/main/java/com/example/android_gimnasio/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android_gimnasio/presentation/MainActivity.kt
@@ -1,33 +1,48 @@
 package com.example.android_gimnasio.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.android_gimnasio.presentation.components.LogearsePantalla
-import com.example.android_gimnasio.ui.theme.AndroidgimnasioTheme
+import com.example.android_gimnasio.presentation.main.PrincipalActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController, startDestination = Screen.Logearse.route) {
-                composable(Screen.Logearse.route) {
-                    LogearsePantalla(
-                        onClickButtonOne = {
-                            navController.navigate(Screen.Registrar.route)
-                        },
-                        onClickButtonTwo = {},
-                        onClickButtonThree = {}
-                    )
-                }
-            }
+            MainScreen()
         }
     }
+}
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = Screen.Logearse.route) {
+        composable(Screen.Logearse.route) {
+            LogearsePantalla(
+                onClickButtonOne = {
+                    context.startActivity(Intent(context, PrincipalActivity::class.java))
+//                            navController.navigate(Screen.Registrar.route)
+                },
+                onClickButtonTwo = {},
+                onClickButtonThree = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
 
