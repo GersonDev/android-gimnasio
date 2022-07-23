4930cd63e41843f2df967ee9b591c8df48cdc502
Carlos Vargas Huaman <cvargas@godaddy.com>
2022-07-22 22:57:47 -0500

    Vertical scroll y horizontal scroll

--- app/src/main/java/com/example/android_gimnasio/presentation/main/components/HomePantalla.kt
+++ app/src/main/java/com/example/android_gimnasio/presentation/main/components/HomePantalla.kt
@@ -1,14 +1,14 @@
 package com.example.android_gimnasio.presentation.main.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,9 +27,32 @@ import com.example.android_gimnasio.R
 
 @Composable
 fun HomePantalla() {
-    Column {
-        Text("Today Workout Plan")
-        TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson", "carlos", "ronaldo", "gerson"))
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(Color.Blue)
+    ) {
+        Column {
+            Text("Today Workout Plan")
+            TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson", "carlos", "ronaldo", "gerson"))
+        }
+
+        Column {
+            Text("Today Workout Plan")
+            TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson", "carlos", "ronaldo", "gerson"))
+        }
+
+        Column {
+            Text("Today Workout Plan")
+            TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson", "carlos", "ronaldo", "gerson"))
+        }
+
+        Column {
+            Text("Today Workout Plan")
+            TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson", "carlos", "ronaldo", "gerson"))
+        }
     }
 }
 
@@ -39,17 +62,32 @@ private fun TodayWorkOutPlan(messages: List<String>) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp)
             .horizontalScroll(rememberScrollState())
+            .padding(start = 10.dp)
     ) {
         messages.forEach { _ ->
-            Card {
-                Box {
+            Card(
+                modifier = Modifier
+                    .width(140.dp)
+                    .padding(10.dp),
+                shape = RoundedCornerShape(15.dp)
+            ) {
+                Box(
+                    contentAlignment = Alignment.BottomCenter
+                ) {
                     Image(
-                        modifier = Modifier.size(100.dp),
+                        modifier = Modifier.fillMaxWidth(),
                         painter = painterResource(id = R.drawable.imagen_jogging),
                         contentDescription = "Imagen de login",
                         contentScale = ContentScale.FillWidth
+                    )
+                    Text(
+                        "Jogging",
+                        color = Color.White,
+                        modifier = Modifier.padding(10.dp),
+                        style = MaterialTheme.typography.body1,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 13.sp,
                     )
                 }
             }
@@ -60,5 +98,5 @@ private fun TodayWorkOutPlan(messages: List<String>) {
 @Preview
 @Composable
 private fun HomePantallaPreview() {
-    TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson"))
+    TodayWorkOutPlan(listOf("carlos"))
 }
