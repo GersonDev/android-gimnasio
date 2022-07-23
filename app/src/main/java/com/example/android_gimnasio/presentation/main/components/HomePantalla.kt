4409c7638450b6d1ddf5ab900b1550d5919a72b1
Carlos Vargas Huaman <cvargas@godaddy.com>
2022-07-22 22:32:12 -0500

    Agregar lista horizontal y scroll horizontal de tarjetas

--- app/src/main/java/com/example/android_gimnasio/presentation/main/components/HomePantalla.kt
+++ app/src/main/java/com/example/android_gimnasio/presentation/main/components/HomePantalla.kt
@@ -1,42 +1,64 @@
 package com.example.android_gimnasio.presentation.main.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.android_gimnasio.R
 
 @Composable
 fun HomePantalla() {
-    Column(
+    Column {
+        Text("Today Workout Plan")
+        TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson", "carlos", "ronaldo", "gerson"))
+    }
+}
+
+@Composable
+private fun TodayWorkOutPlan(messages: List<String>) {
+
+    Row(
         modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700))
-            .wrapContentSize(Alignment.Center)
+            .fillMaxWidth()
+            .height(200.dp)
+            .horizontalScroll(rememberScrollState())
     ) {
-        Text(
-            text = "Home Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        messages.forEach { _ ->
+            Card {
+                Box {
+                    Image(
+                        modifier = Modifier.size(100.dp),
+                        painter = painterResource(id = R.drawable.imagen_jogging),
+                        contentDescription = "Imagen de login",
+                        contentScale = ContentScale.FillWidth
+                    )
+                }
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 private fun HomePantallaPreview() {
-    HomePantalla()
+    TodayWorkOutPlan(listOf("carlos", "ronaldo", "gerson"))
 }
